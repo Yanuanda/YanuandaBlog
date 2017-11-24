675a9a5c17bf30cea27fccaee4f8f5ef9416c2fd
Indra Yanuanda <32921956+Yanuanda@users.noreply.github.com>
2017-11-25 02:43:14 +0700

    Delete YanuandaBlog5.js

--- YanuandaBlog5.js
+++
@@ -1,6 +0,0 @@
-var postperpage=POST_PER_PAGE;
-var numshowpage=5;
-var upPageWord =&#39;Prev&#39;;
-var downPageWord =&#39;Next&#39;;
-var urlactivepage=location.href;
-var home_page=&quot;/&quot;;
